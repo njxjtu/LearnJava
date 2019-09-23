03e174b7ad7e4e49ed552481229b189d0223ca83
njxjtu <njxjtu@gmail.com>
2019-09-23 12:53:04 -0700

    Update MergeTwoSortedLists.java

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -9,25 +9,49 @@ Output: 1->1->2->3->4->4
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode newHead = null, curr = null, curr1 = l1, curr2 = l2;
+        if(l1==null){return l2;}
+        if(l2==null){return l1;}
         
         while(curr1!=null && curr2!=null){
         	ListNode temp = null;
         	if(curr1.val<=curr2.val){
-        		temp.val = curr1.val;
+        		temp = curr1;
         		curr1 = curr1.next;
         	}
         	else{
-        		temp.val = curr2.val;
+        		temp = curr2;
         		curr2 = curr2.next;
         	}
-        	if(newHead==null){
-        		newHead = temp;
-        		curr = newHead;
+        	System.out.println("temp: "+temp.val);
+        	if(curr == null){
+        		curr = temp;
         	}
         	else{
         		curr.next = temp;
+        		curr = curr.next;
+        	}
+        	if(newHead == null){
+        		newHead = curr;
         	}
         }
+        
+        while(curr1!=null){
+        	ListNode temp = null;
+        	System.out.println("curr1!=null");
+        	temp = curr1;
+        	curr.next = temp;
+        	curr = curr.next;
+        	curr1 = curr1.next;
+        }
+        while(curr2!=null){
+        	ListNode temp = null;
+        	System.out.println("curr2!=null");
+        	temp = curr2;
+        	curr.next = temp;
+        	curr = curr.next;
+        	curr2 = curr2.next;
+        }
+        
         return newHead;
     }
 }
