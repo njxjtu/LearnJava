f0b2f335f547b6edcccfdff8fb57b1e620348c7a
njxjtu <njxjtu@gmail.com>
2019-10-01 15:24:51 -0700

    Update SwapNodesInPairs.java

--- SwapNodesInPairs.java
+++ SwapNodesInPairs.java
@@ -30,35 +30,28 @@ public class SwapNodesInPairs {
     		head.next = null;
     		return ln;
     	}
-// Three items
-    	if(head != null && head.next != null && head.next.next != null && head.next.next.next == null){
-    		System.out.println("3 items");
-    		ListNode temp = head.next;
-    		ln = temp;
-    		ln.next = head;
-    		ln.next.next = head.next.next;
-    		return ln;
-    	}
-// More than three items
-    	if(head != null && head.next != null && head.next.next != null && head.next.next.next != null){
-    		ln = head.next;
-    		ln.next = head;
-    	//	ln.next.next = swapPairs(head.next.next);
-    		return ln;
+// More than 2 items
+    	if(head != null && head.next != null && head.next.next !=null){
+    		ListNode temp = swapPairs(head.next.next);
+    		curr1 = head.next;
+    		curr2 = head;
+    		curr1.next = curr2;
+    		curr2.next = temp;
+    		ln = curr1;
     	}
     	return ln;
     }
 
     public static void main(String[] args){
     	SwapNodesInPairs obj = new SwapNodesInPairs();
-    	ListNode n0 = new ListNode(0), n1 = new ListNode(1), n2=new ListNode(2), curr = n0; //, n3=new ListNode(3) ;
-    	n0.next = n1; n1.next = n2; // n2.next = n3; 
+    	ListNode n0 = new ListNode(0), curr = n0, n1 = new ListNode(1), n2=new ListNode(2), n3=new ListNode(3) ;
+    	 n0.next = n1; n1.next = n2;  n2.next = n3; 
     	
     	while(curr !=null){
     		System.out.println(curr.val+" -> ");
     		curr = curr.next;
     	}
-    	
+    	System.out.println(" ------------- ");
     	curr = obj.swapPairs(n0);
     	while(curr !=null){
     		System.out.println(curr.val+" -> ");
