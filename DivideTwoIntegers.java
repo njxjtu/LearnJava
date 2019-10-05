34032020e5bc76f487848ee44b59e3b27ae225ef
njxjtu <njxjtu@gmail.com>
2019-10-04 22:34:42 -0700

    Update DivideTwoIntegers.java

--- DivideTwoIntegers.java
+++ DivideTwoIntegers.java
@@ -13,9 +13,15 @@ Example 2:
 
 Input: dividend = 7, divisor = -3
 Output: -2
+
 Note:
 
 Both dividend and divisor will be 32-bit signed integers.
 The divisor will never be 0.
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
 */
+class DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        
+    }
+}
