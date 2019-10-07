520c598b644be2476b9094a064974496aed7692a
njxjtu <njxjtu@gmail.com>
2019-10-07 14:18:10 -0700

    Update DivideTwoIntegers.java

--- DivideTwoIntegers.java
+++ DivideTwoIntegers.java
@@ -24,6 +24,34 @@ result overflows.
 */
 class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
-        
+    	int count = 0;
+    	int a = dividend>0 ? dividend : -dividend, b = divisor>0 ? divisor : -divisor;
+
+    	if(divisor == -1 && dividend==Integer.MIN_VALUE){
+    		return Integer.MAX_VALUE;
+    	}
+    	else if(divisor == 1 && dividend==Integer.MIN_VALUE){
+    		return Integer.MIN_VALUE;
+    	}
+        else{
+    		if(a>b){ 
+    			count = divide(a-b, b)+1; 
+    			}
+    		else if(a==b){
+    			count = 1 ;
+    		}
+    	}
+        if(dividend>0 && divisor>0 || dividend<0&&divisor<0){
+        	return count;
+        }
+        else{
+        	return -count;
+        }
+    }
+    
+    public static void main(String[] args){
+    	DivideTwoIntegers obj = new DivideTwoIntegers();
+    	System.out.println(2147483647/2);
+    	System.out.println(obj.divide(2147483647,2));
     }
 }
