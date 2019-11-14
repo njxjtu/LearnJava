7a85a7d2b66e39292543e0209ec403b82e93fa26
njxjtu <njxjtu@gmail.com>
2019-11-14 11:15:25 -0800

    Update CombinationSum2.java

--- CombinationSum2.java
+++ CombinationSum2.java
@@ -46,6 +46,10 @@ public class CombinationSum2 {
     	}
     	else if(candidates.length>1){
     		Arrays.sort(candidates); //2, 3
+    		for(int k=0; k<candidates.length;k++){
+    			System.out.print(candidates[k]+", ");
+    		}
+    		System.out.println(" ");
     		int pos = 0, pos2=0, sum=0;
     		while(pos<candidates.length){
     			List<List<Integer>> tempSetOfList = new ArrayList<List<Integer>>();
@@ -56,21 +60,48 @@ public class CombinationSum2 {
     				pos2++;
     			}
     			int[] tempcandi = Arrays.copyOfRange(candidates, pos2, candidates.length);
+    			System.out.println("tempcandi: ");
+    			for(int k=0; k<tempcandi.length;k++){
+        			System.out.print(tempcandi[k]+", ");
+        		}
+        		System.out.println(" ");
     			System.out.println("pos: "+pos+" pos2: "+pos2);
     			while(pos<pos2){
     				System.out.println("in while of pos<pos2");
-    				
-        			tempSetOfList = combinationSum2(tempcandi, target-sum);
-        			
-    				for(List<Integer> tli : tempSetOfList){
-        				tli.addAll(tempList);
-        				setOfList.add(tli);
+    				tempSetOfList = combinationSum2(tempcandi, target-sum);
+    				if(tempSetOfList.isEmpty()){
+        				System.out.println("tempSetOfList is empty - 1");
         			}
+    				else{
+    					for(List<Integer> tli : tempSetOfList){
+            				tli.addAll(tempList);
+            				setOfList.add(tli);
+            			}
+    				}
     				
     				tempList.add(candidates[pos]);
         			sum = sum + candidates[pos];
+        			tempSetOfList = combinationSum2(tempcandi, target-sum);
+        			if(tempSetOfList.isEmpty()){
+        				System.out.println("tempSetOfList is empty - 2");
+        				tempList.add(candidates[pos]);
+        			}
+        			else{
+        				for(List<Integer> tli : tempSetOfList){
+        					System.out.println("In the second for loop");
+            				tli.addAll(tempList);
+            				setOfList.add(tli);
+            			}
+        			}
+        			if(tempSetOfList.isEmpty() && target==candidates[pos]){
+        				tempList.add(candidates[pos]); 
+        				setOfList.add(tempList);
+        				break;
+        			}
+    				
         			pos++;
     			}
+    			setOfList.add(tempList);
     			pos++;
     		}
     		
@@ -81,8 +112,8 @@ public class CombinationSum2 {
     public static void main(String[] args){
     	CombinationSum2 obj = new CombinationSum2();
     	List<List<Integer>> setOfList = new ArrayList<List<Integer>>();
-    	int[] candidates = {2,3};
-    	int target = 5;
+    	int[] candidates = {2,2};
+    	int target = 4;
     	setOfList = obj.combinationSum2(candidates, target);
     	for(List<Integer> li : setOfList){
     		for(int i : li){
