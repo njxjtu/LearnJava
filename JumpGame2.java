bb5b909426a66fec39e7f011f9a87c521dc06602
njxjtu <njxjtu@gmail.com>
2020-01-21 14:51:40 -0800

    Update JumpGame2.java

--- JumpGame2.java
+++ JumpGame2.java
@@ -20,25 +20,51 @@ Note:
 You can assume that you can always reach the last index.
  */
 public class JumpGame2 {
-    public int jump(int[] nums) {
+    /*public int jump(int[] nums) {
     	if(nums.length<=1){
     		return 0;
     	}
     	else{
     		int maxc =nums.length-1;
-            for(int i=1; i<=nums[0]; i++){
-            	if(i+jump(Arrays.copyOfRange(nums,i,nums.length))<=maxc){
-            		maxc = i+jump(Arrays.copyOfRange(nums,i,nums.length));
+            for(int i=1; i<=nums[0] && i<nums.length; i++){
+            	int restjump = jump(Arrays.copyOfRange(nums,i,nums.length));
+            	if(1+restjump<=maxc){
+            		maxc = 1+restjump;
             	};
             }
             return maxc;
     	}
         
+    }*/
+	
+	public int jump(int[] nums) {
+		int maxc=nums.length, tablen = nums.length; 
+		
+    	boolean tab[][] = new boolean[nums.length][nums.length];
+    	//set the tab table to false in every cell
+    	for(int i=0; i<tablen; i++){
+    		for(int j=0; j<tablen; j++){
+    			tab[i][j]=false;
+    		}
+    	}
+    	for(int i=0; i<tablen; i++){
+    		for(int j=i+1; j<=i+nums[i] && j<tablen; j++){
+    			tab[i][j]=true;
+    		}
+    	}	
+    	for(int i=0; i<tablen; i++){
+    		if(tab[i][tablen-1]){
+    			return i+1;
+    		}
+    	}
+        return maxc;
     }
     
   public static void main(String[] args){
-	  int[] nums = {2,3,1,1,4};
+	  //int[] nums = {2,3,1,1,4};
+	  int[] nums = {2,3,1,1};
+	  //int[] nums = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
 	  JumpGame2 obj = new JumpGame2();
-	  System.out.println(obj.jump(nums));
+	  System.out.println("array length: "+nums.length+" jump number: "+obj.jump(nums));
   }
 }
