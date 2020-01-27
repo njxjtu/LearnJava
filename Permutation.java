dd00a4b16466c62b627d390c9b283ceea012e003
njxjtu <njxjtu@gmail.com>
2020-01-27 10:43:54 -0800

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -6,7 +6,6 @@ import java.util.*;
 Given a collection of distinct integers, return all possible permutations.
 
 Example:
-
 Input: [1,2,3]
 Output:
 [
@@ -17,19 +16,25 @@ Output:
   [3,1,2],
   [3,2,1]
 ]
+
+Example:
+Input: []
+Output:
+[[]]
  */
 public class Permutation {
     public List<List<Integer>> permute(int[] nums) {
     	List<List<Integer>> lli = new ArrayList<List<Integer>>();
-    	
-    	if(nums.length==0){return lli;}
-    	if(nums.length==1){
+
+    	if(nums.length<=1){
     		List<Integer> li = new ArrayList<Integer>();
-    		li.add(nums[0]);
+    		if(nums.length==1){
+    			li.add(nums[0]);
+    		}
     		lli.add(li);
     		return lli;
         }
-    	else {
+    	if(nums.length>1){
     		for(int i=0; i<nums.length; i++){
     			int[] temparr = new int[nums.length-1];
     			int j=0;
@@ -39,9 +44,10 @@ public class Permutation {
         				j++;
         			}
         		}
-        		lli = permute(temparr);
-        		for(List<Integer> x : lli){
+        		List<List<Integer>> templli = permute(temparr);
+        		for(List<Integer> x : templli){
         			x.add(nums[i]);
+        			lli.add(x);
         		}
         	}
     	}
@@ -51,12 +57,14 @@ public class Permutation {
     
     public static void main(String[] args){
     	Permutation obj = new Permutation();
-    	int[] nums = {4,5};
+    	//int[] nums = {4,6};
+    	//int[] nums = {1,2,3};
+    	int[] nums = {};
     	List<List<Integer>> rlli =  obj.permute(nums);
     	for(int i=0; i<rlli.size(); i++){
     		System.out.println("");
     		for(int j=0; j<nums.length; j++){
-    			System.out.println(rlli.get(i).get(j)+", ");
+    			System.out.print(rlli.get(i).get(j)+", ");
     		}
     	}
     	
