1b666a6dd479cd33e4e854865db2c0de3b45223f
njxjtu <njxjtu@gmail.com>
2020-02-10 15:39:53 -0800

    Create PermutationsII.java

---
+++ PermutationsII.java
@@ -0,0 +1,19 @@
+package LearnJava;
+
+/*Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+	*/	
+public class PermutationsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+    }
+}
