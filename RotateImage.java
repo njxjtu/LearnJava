41a7c36e9d02200e7d6f32289c0b21d369939abe
njxjtu <njxjtu@gmail.com>
2020-02-12 14:17:21 -0800

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -40,9 +40,42 @@ rotate the input matrix in-place such that it becomes:
   [12, 6, 8, 9],
   [16, 7,10,11]
 ]
+
+Example 3:
+Input
+[[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output
+[[15,2,13,5],[14,4,3,1],[12,8,6,9],[16,10,7,11]]
+Expected
+[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 		*/
 public class RotateImage {
     public void rotate(int[][] matrix) {
-        
+    	int n = matrix.length, tempint;
+        int[] temprow = new int[n];
+        for(int i=0; i<n/2; i++){
+        		temprow = matrix[i];
+        		matrix[i] = matrix[n-i-1];
+        		matrix[n-i-1] = temprow;
+        }
+        for(int i=0; i<n; i++){
+    		for(int j=0; j<=i; j++){
+    			tempint = matrix[i][j];
+    			matrix[i][j] = matrix[j][i];
+    			matrix[j][i] = tempint;
+    		}
+    	}
+    }
+    
+    public static void main(String[] args){
+    	int[][] matrix = {{5,1,9,11},{2, 4, 8,10},{13, 3, 6, 7},{15,14,12,16}};
+    	RotateImage obj = new RotateImage();
+    	obj.rotate(matrix);
+    	for(int i=0; i<matrix.length; i++){
+    		System.out.println("");
+    		for(int j=0; j<matrix.length; j++){
+    			System.out.print(matrix[i][j]+", ");
+    		}
+    	}
     }
 }
