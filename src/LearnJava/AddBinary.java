18c144a88b7d9b76d7f2f778702d508b8de534cd
Nan Jiang <njxjtu@gmail.com>
2025-08-20 10:48:15 -0700

    Add Binary

---
+++ src/LearnJava/AddBinary.java
@@ -0,0 +1,56 @@
+package LearnJava;
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+
+ */
+public class AddBinary {
+
+    public String add(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinary ab = new AddBinary();
+        String c = ab.add("1010", "1011");
+        System.out.println(c);
+    }
+    
+}
