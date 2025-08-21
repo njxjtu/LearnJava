b280dad2c5855cf40a08243578a11f5539e7da12
Nan Jiang <njxjtu@gmail.com>
2025-08-21 12:21:21 -0700

    Square Root

--- src/LearnJava/SquareRoot.java
+++ src/LearnJava/SquareRoot.java
@@ -6,5 +6,29 @@ You must not use any built-in exponent function or operator.
 For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
  */
 public class SquareRoot {
+
+    public int mySqrt(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("Input must be a non-negative integer.");
+        }
+        if (x == 0) {
+            return 0;
+        }
+
+        long left = 1;
+        long right = x;
+        long ans = 0;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int) ans;
+    }
     
 }
