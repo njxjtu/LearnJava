392e73d4d8e86cd85fbcad551928410b0fe3ac84
Nan Jiang <njxjtu@gmail.com>
2025-08-22 10:17:26 -0700

    Square Root

--- src/LearnJava/SquareRoot.java
+++ src/LearnJava/SquareRoot.java
@@ -31,9 +31,37 @@ public class SquareRoot {
         return (int) ans;
     }
 
+    public int mySqrt2(int x) {
+
+        if (x < 0 ) throw new IllegalArgumentException("Input must be a non-negative integer.");
+
+        else if (x == 0) return 0;
+        
+        else {
+            for (int i = 1; i<= x; i++) {
+                if ( i*i == x) return i;
+                else if ((i+1)*(i+1) == x) return i+1;
+                else if (i*i <= x && (i+1)*(i+1) >=x) {
+                    return i;
+                }
+            }
+        }
+        return x;
+    }
     public static void main(String[] args) {
         SquareRoot sr = new SquareRoot();
-        System.out.println(sr.mySqrt(19));
+        int input = 4;
+        System.out.println(sr.mySqrt(input));
+        System.out.println(sr.mySqrt2(input));
+        input = 19;
+        System.out.println(sr.mySqrt(input));
+        System.out.println(sr.mySqrt2(input));
+        input = 8;
+        System.out.println(sr.mySqrt(input));
+        System.out.println(sr.mySqrt2(input));
+        input = 2147483647;
+        System.out.println(sr.mySqrt(input));
+        System.out.println(sr.mySqrt2(input));
     }
     
 }
