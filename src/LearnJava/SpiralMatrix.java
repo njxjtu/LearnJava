782dc74789897d11935edbb79bb6185a01b725d0
Nan Jiang <njxjtu@gmail.com>
2025-08-24 18:04:46 -0700

    Spiral Matrix

---
+++ src/LearnJava/SpiralMatrix.java
@@ -0,0 +1,19 @@
+package LearnJava;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/*
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ */
+public class SpiralMatrix {
+    
+        public List<Integer> spiralOrder(int[][] matrix) {
+
+            List<Integer> result = new ArrayList<Integer>();
+
+            return result;
+        
+    }
+    
+}
