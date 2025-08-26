22742f7c22063ac1907fb82a46215ab2eb1a838b
Nan Jiang <njxjtu@gmail.com>
2025-08-25 22:06:19 -0700

    Spiral Matrix

--- src/LearnJava/SpiralMatrix.java
+++ src/LearnJava/SpiralMatrix.java
@@ -11,6 +11,29 @@ public class SpiralMatrix {
         public List<Integer> spiralOrder(int[][] matrix) {
 
             List<Integer> result = new ArrayList<Integer>();
+            int m=matrix.length;
+            int n=matrix[0].length;
+            if(m==0)
+            return result;
+            if(n==0)
+            return result;
+            if(m==1)
+            {
+                for(int i=0; i<n; i++)
+                result.add(matrix[0][i]);
+                return result;
+            }
+            if(n==1)
+            {
+                for(int i=0; i<m; i++)
+                result.add(matrix[i][0]);
+                return result;
+            }
+
+            for(int i=0; i<n; i++)
+            result.add(matrix[0][i]);
+            for(int i=1; i<m; i++)
+            result.add(matrix[i][n-1]);
 
             return result;
         
