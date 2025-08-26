25dd4d664f08e306071c978217551631e39fec58
Nan Jiang <njxjtu@gmail.com>
2025-08-26 13:45:50 -0700

    Spriral Matrix

--- src/LearnJava/SpiralMatrix.java
+++ src/LearnJava/SpiralMatrix.java
@@ -8,6 +8,26 @@ import java.util.ArrayList;
  */
 public class SpiralMatrix {
     
+    public int[][] rotateMatrix(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        if (m == 0 || n == 0) {
+            return matrix;
+        }
+
+        int[][] result = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][m - 1 - i] = matrix[i][j];
+            }
+        }
+
+        return result;
+
+    }
+
         public List<Integer> spiralOrder(int[][] matrix) {
 
             List<Integer> result = new ArrayList<Integer>();
@@ -32,8 +52,15 @@ public class SpiralMatrix {
 
             for(int i=0; i<n; i++)
             result.add(matrix[0][i]);
+
+            int[][] temp = new int[m-1][n];
             for(int i=1; i<m; i++)
-            result.add(matrix[i][n-1]);
+            {
+                for(int j=0; j<n; j++)
+                temp[i-1][j]=matrix[i][j];
+            }
+            
+            result.addAll(spiralOrder(rotateMatrix(temp)));
 
             return result;
         
