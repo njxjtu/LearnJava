492a0ec754ded8509022168733cd4ea4eab5cd55
Nan Jiang <njxjtu@gmail.com>
2025-08-27 10:24:30 -0700

    Spiral Matrix

--- src/LearnJava/SpiralMatrix.java
+++ src/LearnJava/SpiralMatrix.java
@@ -1,6 +1,7 @@
 package LearnJava;
 
 import java.util.List;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -55,6 +56,29 @@ public class SpiralMatrix {
                 return result;
             }
 
+            int col = 0;
+            for (int i=0; i<=m/2; i++) {
+                for (int j=col; j<=n-col-1; j++){
+                    result.add(matrix[i][j]);
+                }
+                for (int k=i+1; k<=m-i-1; k++) {
+                    result.add(matrix[k][n-col-1]);
+                }
+                for (int l=n-col-2; l>=col; l--) {
+                    result.add(matrix[m-i-1][l]);
+                }
+                for (int p=m-i-2; p>i; p--) {
+                    result.add(matrix[p][col]);
+                }
+
+                System.out.println("When i="+i+", result="+result+", col="+col+" m/2="+m/2);
+
+                col++;
+                if(col>n/2){
+                    break;
+                }
+            }
+
 
 
             return result;
@@ -65,11 +89,17 @@ public class SpiralMatrix {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
         SpiralMatrix sm = new SpiralMatrix();
         List<Integer> result = sm.spiralOrder(matrix);
+        System.out.println(result);
 
-        for (int i = 0; i < result.size(); i++) {
-            System.out.print(result.get(i) + " ");
-        }
-        System.out.println();
+        int[][] matrix2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        SpiralMatrix sm2 = new SpiralMatrix();
+        List<Integer> result2 = sm2.spiralOrder(matrix2);
+        System.out.println(result2);
+
+        int[][] matrix3 = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        SpiralMatrix sm3 = new SpiralMatrix();
+        List<Integer> result3 = sm3 .spiralOrder(matrix3);
+        System.out.println(result3);
     }
     
 }
