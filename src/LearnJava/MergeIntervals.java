27a752056fe86437b2f98451729ea012a0dd1cfb
Nan Jiang <njxjtu@gmail.com>
2025-08-27 17:04:50 -0700

    Merge intervals

---
+++ src/LearnJava/MergeIntervals.java
@@ -0,0 +1,8 @@
+package LearnJava;
+/*
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
+public class MergeIntervals {
+    
+}
