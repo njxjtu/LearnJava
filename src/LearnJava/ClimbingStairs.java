20fbdd60481638d6d0c4bc224cf836399cfff464
Nan Jiang <njxjtu@gmail.com>
2025-08-28 22:10:29 -0700

    Climbing Stairs

---
+++ src/LearnJava/ClimbingStairs.java
@@ -0,0 +1,11 @@
+package LearnJava;
+
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+1 <= n <= 45
+ */
+
+public class ClimbingStairs {
+    
+}
