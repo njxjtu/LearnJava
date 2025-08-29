72b29e6736374074a943b193cee0dc75a8fe2aab
Nan Jiang <njxjtu@gmail.com>
2025-08-28 22:26:41 -0700

    Climbing Stairs

--- src/LearnJava/ClimbingStairs.java
+++ src/LearnJava/ClimbingStairs.java
@@ -7,5 +7,24 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
  */
 
 public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     
+    public static void main(String[] args) {
+        ClimbingStairs cs = new ClimbingStairs();
+        System.out.println(cs.climbStairs(3));
+        System.out.println(cs.climbStairs(4));
+    }
 }
